5f62d8f3049cfa036124ec650701decf2e68c9d8
Hyun Frantz <hyunfrantz@gmail.com>
2017-04-06 23:58:45 -0400

    layout test

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 3
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+  end
+end
+
+
+# Get the root path (Home page).
+# Verify that the right page template is rendered.
+# Check for the correct links to the Home, Help, About, and Contact pages.
